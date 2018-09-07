2f5456b769bd8e782ba2acf91332fa8c25b9d4ca
Rupak Majumdar <rupak@mpi-sws.org>
2018-09-07 19:12:16 +0200

    modified:   __init__.py 	modified:   hashtree.py 	modified:   local_file_resource.py 	modified:   ../../tests/testcli.sh

--- dataworkspaces/resources/local_file_resource.py
+++ dataworkspaces/resources/local_file_resource.py
@@ -2,12 +2,14 @@
 Resource for files living in a local directory 
 """
 import subprocess
+from errno import EEXIST
 import os
 import os.path 
 
 from dataworkspaces.errors import ConfigurationError
 import dataworkspaces.commands.actions as actions
 from .resource import Resource, ResourceFactory
+from . import hashtree
 
 LOCAL_FILE = 'file'
 
@@ -30,24 +32,26 @@ class LocalFileResource(Resource):
             raise ConfigurationError(self.local_path + ' does not exist')
         if not(os.access(self.local_path, os.R_OK)): 
             raise ConfigurationError(self.local_path + ' does not have read permission')
-        if realpath(self.local_path)==realpath(self.workspace_dir):
+        if os.path.realpath(self.local_path)==os.path.realpath(self.workspace_dir):
             raise ConfigurationError("Cannot add the entire workspace as a file resource")
 
 
     def add(self):
         try:
-            rsrcdir = os.path.abspath(self.workspace_dir / 'resources' / self.role / self.local_path)
+            rsrcdir = os.path.abspath(self.workspace_dir + 'resources/' + self.role + '/' + self.local_path)
             os.makedirs(rsrcdir)
         except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(rsrcdir):
+            if exc.errno == EEXIST and os.path.isdir(rsrcdir):
                 pass
             else: raise
 
-    def snapshot_prechecks(self, dws_basedir):
+    def snapshot_prechecks(self):
         pass
 
-    def snapshot(self, dws_basedir):
-        pass
+    def snapshot(self):
+        rsrcdir = os.path.abspath(self.workspace_dir + 'resources/' + self.role + '/' + self.local_path)
+        h = hashtree.generate_hashes(rsrcdir, self.local_path)
+        return h.strip()
 
     def __str__(self):
         return "Local directory %s in role '%s'" % (self.local_path, self.role)
@@ -56,11 +60,11 @@ class LocalFileFactory(ResourceFactory):
     def from_command_line(self, role, workspace_dir, batch, verbose,
                           local_path):
         """Instantiate a resource object from the add command's arguments"""
-        url = LOCAL_FILE + local_path
+        url = LOCAL_FILE + ':' + local_path
         return LocalFileResource(url, role, workspace_dir, local_path)
 
     def from_json(self, json_data, workspace_dir, batch, verbose):
         """Instantiate a resource object from the parsed resources.json file"""
         assert json_data['resource_type']==LOCAL_FILE
-        return GitRepoResource(json_data['url'], json_data['role'], workspace_dir, json_data['local_path'])
+        return LocalFileResource(json_data['url'], json_data['role'], workspace_dir, json_data['local_path'])
 
