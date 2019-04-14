6e8cd98e06c147042b9e9c6a7f4999636bf6161b
Jeff Fischer <jeff@data-ken.org>
2019-04-13 17:02:29 -0700

    Mock out dependencies for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = [
     'sphinx_click.ext'
 ]
 
-autodoc_mock_imports=['click']
+autodoc_mock_imports=['click', 'sklearn', 'ipykernel', 'notebook']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
