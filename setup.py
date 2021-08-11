33f9ed7cd00b1e5ba8d66b00d9de6de644dbcc7c
Jeff Fischer <jeff@data-ken.org>
2021-08-10 20:54:32 -0700

    modernize the packaging

--- setup.py
+++ setup.py
@@ -1,43 +1,51 @@
-# Copyright 2018,2019 by MPI-SWS and Data-ken Research. Licensed under Apache 2.0. See LICENSE.txt.
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# Copyright 2018-2021 by MPI-SWS and Benedat LLC. Licensed under Apache 2.0. See LICENSE.txt.
 
-import sys
-sys.path.insert(0, 'dataworkspaces')
-from dataworkspaces import __version__
+import setuptools
 
-with open("README.rst", "r") as f:
-    long_description = f.read()
-setup(
-    name='dataworkspaces',
-    version=__version__,
-    author="Max Plack Institute for Software Systems, Data-ken Research",
-    author_email='jeff@data-ken.org',
-    description="Easy management of source data, intermediate data, and results for data science projects",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/data-workspaces/data-workspaces-core",
-    packages=find_packages(),
-    include_package_data=True, # needed for copying data files at install time
-    python_requires=">=3.6",
-    install_requires=[
-        'click',
-        'requests',
-        'dws-repo2docker'
-    ],
-    entry_points="""
-        [console_scripts]
-        dws=dataworkspaces.__main__:main
-        git-fat=dataworkspaces.third_party.git_fat:main
-    """,
-    classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows :: Windows 10",
-        "Topic :: Software Development :: Version Control",
-        "Topic :: Scientific/Engineering :: Information Analysis"
-    ]
-)
+if __name__ == "__main__":
+    setuptools.setup()
+
+# Old non-declarative version - see setup.cfg for latest
+# from setuptools import setup, find_packages
+
+# import sys
+# sys.path.insert(0, 'dataworkspaces')
+# from dataworkspaces import __version__
+
+# with open("README.rst", "r") as f:
+#     long_description = f.read()
+# setup(
+#     name='dataworkspaces',
+#     version=__version__,
+#     author="Max Plack Institute for Software Systems, Data-ken Research",
+#     author_email='jeff@data-ken.org',
+#     description="Easy management of source data, intermediate data, and results for data science projects",
+#     long_description=long_description,
+#     long_description_content_type="text/x-rst",
+#     url="https://github.com/data-workspaces/data-workspaces-core",
+#     packages=find_packages(),
+#     include_package_data=True, # needed for copying data files at install time
+#     python_requires=">=3.6",
+#     install_requires=[
+#         'click',
+#         'requests',
+#         'dws-repo2docker'
+#     ],
+#     entry_points="""
+#         [console_scripts]
+#         dws=dataworkspaces.__main__:main
+#         git-fat=dataworkspaces.third_party.git_fat:main
+#     """,
+#     classifiers=[
+#         "Programming Language :: Python :: 3 :: Only",
+#         "Development Status :: 4 - Beta",
+#         "License :: OSI Approved :: Apache Software License",
+#         "Operating System :: MacOS",
+#         "Operating System :: POSIX :: Linux",
+#         "Operating System :: POSIX",
+#         "Operating System :: Microsoft :: Windows :: Windows 10",
+#         "Topic :: Software Development :: Version Control",
+#         "Topic :: Scientific/Engineering :: Information Analysis"
+#     ]
+# )
